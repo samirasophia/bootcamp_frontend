c8567c223ac2e8c145cd0f86cc2ee89b9d89ac70
samirasophia <samira.tesan@coop.ch>
2024-02-27 10:27:48 +0100

    .

---
+++ .idea/einführung.js
@@ -0,0 +1,71 @@
+/*
+
+
+// Variabeln
+
+var foo: string = 'a';
+let foo2: number = 10;
+foo = 2;
+
+//konstanten
+
+const FIXED: string = 'fixed';
+
+//operationen
+
+console.log(1 + 1);
+
+//vergleichsoperationen
+
+console.log(1 == true);
+console.log(1 === true);
+
+//logische operatoren
+
+console.log(true && true);
+console.log(true || true);
+console.log(!true);
+
+//zuweisungsoperatoren
+
+let op: number = 2;
+cosole.log(op);
+op += 2;
+
+//arrays
+
+const arr: String[] = ['meow', 'meow', 'meow', 2, true];
+const.log(arr[0]);
+
+//objekte
+
+const obj: {job:string, name:string} = {
+    name: 'value',
+    job: 'job',
+}
+
+obj['job'] = 'meow';
+console.log(obj.name);
+
+//for schleifen
+
+for (let i = 0; i<5; i++){
+    console.log(i);
+}
+
+//if else
+
+const age :number = 18;
+if (age<29){
+    console.log("meow")
+} else {
+    console.log("yikez")
+}
+
+//any (mehrere datentypen)
+
+let hello: any = 2;
+hello = ' ';
+hello = true;
+
+*/
